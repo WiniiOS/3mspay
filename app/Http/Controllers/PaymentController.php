d924fecd87d2c1e97fc40e0a45f6294fb7062e47
garaa2n <franckndi5@gmail.com>
2022-04-18 11:51:02 +0100

    Ajout des fonctions de paiement via Adwapay

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -44,4 +44,70 @@ class PaymentController extends Controller
         ]);
 
     }
+
+    // Fonction qui génère un token Adwapay
+    public function get_token(){
+
+        $username = 'BLOO1';
+        $pass = 'BL3HFKUAVBXXO1';
+        $url = 'https://twsv03.adwapay.cm/getADPToken';
+        
+        $curl = curl_init($url);
+      
+        $headers = array(
+           "Authorization: Basic QkxPTzE6QkwzSEZLVUFWQlhYTzE=$username:$pass",
+           "Content-Type:application/json",
+           "Accept: application/json"
+        );
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        echo $response;
+        $result = json_decode($response);
+        return $result->tokenCode;
+      
+    }
+
+    function ticket_pay($operator,$telephone,$transaction_no){
+
+        $token = $this->get_token();
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://twsv03.adwapay.cm/requestToPay',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS =>'{
+          "meanCode": '.$operator.',
+          "paymentNumber": '.$telephone.',
+          "orderNumber": '.$transaction_no.',
+          "amount": 30000,
+          "currency": "XAF",
+          "feesAmount": "30"
+        }
+        ',
+          CURLOPT_HTTPHEADER => array(
+            'AUTH-API-TOKEN: '.$token.'',
+            'AUTH-API-SUBSCRIPTION: BL3HFKUAVBXXO1',
+            'Content-Type: text/plain'
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        echo $response;
+        
+        $result = json_decode($response);
+        return $result;
+       
+  }
+
 }
